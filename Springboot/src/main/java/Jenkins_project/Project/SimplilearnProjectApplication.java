b217b55bae9e942e4dd250aaecdcc165e0d596a7
purusothaman <purus83@gmail.com>
2022-12-26 09:33:40 +0530

    Springboot code base added

---
+++ Springboot/src/main/java/Jenkins_project/Project/SimplilearnProjectApplication.java
@@ -0,0 +1,13 @@
+package Jenkins_project.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnProjectApplication.class, args);
+	}
+
+}
---
+++ Springboot/src/test/java/Jenkins_project/Project/SimplilearnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Jenkins_project.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
